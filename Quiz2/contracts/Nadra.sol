210f1258e1989cdc16bc18493faaec5a1d7e1306
Zahoor Khan <106008372+ZahoorKhan47@users.noreply.github.com>
2022-06-29 00:38:50 +0500

    Create Nadra.sol

---
+++ Quiz2/contracts/Nadra.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+
+
+
+
+contract Nadra{
+
+
+
+    struct Person{
+
+        string name ;
+
+        string cnic;
+
+        address addr;
+
+        string dob;
+
+        uint treenum;
+
+    }
+
+
+
+
+
+    mapping(uint=>Person)public personMap;
+
+    uint[] idArry;
+
+    
+
+
+
+
+
+    function addDetail(
+
+        uint id,
+
+        string memory _name,
+
+        string memory _cnic,
+
+        address _addr,
+
+        string memory _dob,
+
+        uint _treenum
+
+    )public {
+
+        
+
+        idArry.push(id);
+
+        
+
+
+
+        Person storage newPerson = personMap[id];
+
+
+
+        newPerson.name=_name;
+
+        newPerson.cnic=_cnic;
+
+        newPerson.addr=_addr;
+
+        newPerson.dob=_dob;
+
+        newPerson.treenum=_treenum;
+
+        
+
+        
+
+    }
+
+
+
+    function getDetailsByID(uint _id)public view returns(Person memory){
+
+        return personMap[_id];
+
+    }
+
+
+
+
+
+
+
+    function getAllDetails()public view  returns(Person[] memory) {
+
+
+
+        Person[] memory personArr;
+
+
+
+        for(uint i=0 ;i<idArry.length; i++){
+
+
+
+            personArr[i]=personMap[idArry[i]];
+
+
+
+        }
+
+
+
+        return personArr;
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
